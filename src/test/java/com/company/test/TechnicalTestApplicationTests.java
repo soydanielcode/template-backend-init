8157495f478de94264f7059ff78f0d724bf0ec79
soydanielcode <soydanielcode@outlook.com>
2024-02-19 17:28:13 -0500

    Init Project

---
+++ src/test/java/com/company/test/TechnicalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
